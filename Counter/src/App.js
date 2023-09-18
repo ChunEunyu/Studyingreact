30a0cce66359c350d80feb5facafd52627e9c9cb
ChunEunyu <xhoneyx01@naver.com>
2023-09-18 10:22:44 +0900

    카운터 만들기

---
+++ Counter/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+import Wrappers from './Wrappers';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
